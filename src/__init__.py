98fc8c325ac8f1b783ceb74a2d5e079dbe6b21f5
alessandroliafook <alessandro.liafook@gmail.com>
2017-08-19 20:58:30 -0300

    Implement the exceptions of the system #3

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class SyntaxError(Error):
+    """
+        Exception raised for errors in the syntax of the turing machine
+
+        Attributes:
+            menssage -- explanation of the error
+            line -- line that error accur
+            expression -- input expression in with error occurred
+    """
+    def __init__(self, menssage, line):
+        self.line = line
+        self.message = message
+        self.expression = "At line %d. %s" % (self.line + 1, self.message)
