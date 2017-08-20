90c4e714e2c317a0993cbb9b468440cadf9e595d
Nicácio Oliveira de Sousa <nnicacio@gmail.com>
2017-08-19 22:49:05 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,6 @@ class Error(Exception):
     """Base class for exceptions in this module."""
     pass
 
-
 class SyntaxError(Error):
     """
         Exception raised for errors in the syntax of the turing machine
@@ -12,7 +11,7 @@ class SyntaxError(Error):
             line -- line that error accur
             expression -- input expression in with error occurred
     """
-    def __init__(self, menssage, line):
+    def __init__(self, message, line):
         self.line = line
         self.message = message
         self.expression = "At line %d. %s" % (self.line + 1, self.message)
