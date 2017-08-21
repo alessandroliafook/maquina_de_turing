cdd71d3969ddf6aaa19f9f987090677cbfcf0293
alessandroliafook <alessandro.liafook@gmail.com>
2017-08-20 21:44:50 -0300

    Develop structure fot methods of data validation #3

---
+++ src/view/data_validator.py
@@ -0,0 +1,15 @@
+
+def commands(commands):
+    return
+
+
+def states(states):
+    return
+
+
+def input(state, actual_input):
+    return
+
+
+def transition(state, input_symbol, direction_symbol, next_state):
+    return
