2ec7800c614b1a30d30ffaa0030951f65f583438
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2017-08-22 20:26:08 -0300

    Correction in 'configuration' to clear the dictionary

--- src/model/main_tester.py
+++ src/model/main_tester.py
@@ -1,8 +1,14 @@
-
+# noinspection PyUnresolvedReferences
 from machine import Machine
 
 #palindrome_detector
 machine = Machine()
 machine.upload_states_by_archive("../tests/palindrome_detector.in")
-machine.insert_tape("101")
+machine.insert_tape("1001001001001001")
+machine.run()
+
+#Binary addiction
+machine = Machine()
+machine.upload_states_by_archive("../tests/binary_addiction.in")
+machine.insert_tape("10 10")
 machine.run()
