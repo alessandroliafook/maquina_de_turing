68ae189393a695ceb908fdc137ff02159e1b1924
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2017-08-28 18:11:52 -0300

    Refactored and functional - is biaurful :)

--- src/model/machine.py
+++ src/model/machine.py
@@ -17,7 +17,6 @@ class Machine:
     def __init__(self, tape=Tape(), machine_program=Machine_Program()):
         self.tape = tape
         self.machine_program = machine_program
-        self.steps = []
 
     def have_program(self):
         return self.machine_program.have_states()
@@ -30,49 +29,47 @@ class Machine:
         self.tape.insert(input)
 
     def run(self):
-        cont = 0
+        # read initial state
+        steps = 0
         current_state = self.machine_program.next_state(INITIALSTATENAME, self.tape.get_head_symbol())
         while True:
-            time.sleep(0.10)
-            print ("Current state: %s | Head Position: %d | Steps: %d" % (str(current_state),
-                                                                          self.tape.get_head_position(), cont))
-            print ("Tape: \n" + self.tape.get_head_pointer())
-            print (self.tape)
+            #time.sleep(0.3)
             if current_state is not None:
+                self.print_current_configuration(self.tape, current_state.name, steps)
+                self.print_tape(self.tape)
+                steps += 1
+                # write
                 self.tape.write(current_state.new_symbol)
+                # move
                 self.tape.move(current_state.direction)
-                cont += 1
-                step = (current_state.name, cont, self.tape.get_head_pointer(), str(self.tape))
-                self.steps.append(step)
-                if current_state.next_state_name in ACCEPTANCESTATE:
-                    print ("Current state: %s | Head Position: %d | Steps: %d" % (str(current_state),
-                                                                                  self.tape.get_head_position(), cont))
-                    print ("=== Accepted ===")
+                # Verify halt status
+                if self.verify_halt(current_state.next_state_name, self.tape, steps):
                     break
-                elif current_state.next_state_name in BOUNCESTATE:
-                    print ("Current state: %s | Head Position: %d | Steps: %d" % (str(current_state),
-                                                                                  self.tape.get_head_position(), cont))
-                    print ("=== Rejected ===")
-                    break
-
+                # read next state
                 state_aux = current_state
                 current_state = self.machine_program.next_state(state_aux.next_state_name, self.tape.get_head_symbol())
             else:
+                print("========= Halted =========")
                 break
 
-    def run_steps(self):
-        command = raw_input("press n to next step or r to run: ")
-        for step in self.steps:
-            if command.lower() == "n":
-                self.print_step(step)
-                command = raw_input("press n to next step or r to run: ")
-            elif command.lower() == "r":
-                self.print_step(step)
-            else:
-                print ("opcao invalida, vai ate o fim msm")
-                self.print_step(step)
+    def verify_halt(self, next_state_name, tape, steps):
+        # verifies acceptance states
+        if next_state_name == ACCEPTANCESTATE or next_state_name == BOUNCESTATE:
+            self.print_current_configuration(self.tape, next_state_name, steps)
+            self.print_tape(self.tape)
+            print("========= Halted =========")
+            return True
+        elif next_state_name == "halt":
+            self.print_current_configuration(self.tape, next_state_name, steps)
+            self.print_tape(self.tape)
+            print("========= Halted =========")
+            return True
+        else:
+            return False
 
-    def print_step(self, step):
-        print ("Current state: %s | Steps: %d" % (step[0], step[1]))
-        print ("Tape: \n" + step[2])
-        print (step[3])
+    def print_tape(self, tape):
+        print("Tape: \n" + tape.get_head_pointer())
+        print(tape)
+
+    def print_current_configuration(self, tape, current_state_name, steps):
+        print("Current state: %s | Head Position: %d | Steps: %d" % (current_state_name, tape.get_head_position(), steps))
--- src/model/main_tester.py
+++ src/model/main_tester.py
@@ -1,25 +1,26 @@
 # noinspection PyUnresolvedReferences
 from machine import Machine
 
-
+'''
 #palindrome_detector - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/palindrome_detector.in")
+machine.load_program("../tests/palindrome_detector.in")
 machine.insert_tape("1001")
 machine.run()
-
+'''
 
 '''
 #Binary addiction - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/binary_addiction.in")
+machine.load_program("../tests/binary_addiction.in")
 machine.insert_tape("10 10")
 machine.run()
 '''
+
 '''
 #Binary Multiplication - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/binary_multiplication.in")
+machine.load_program("../tests/binary_multiplication.in")
 machine.insert_tape("100 10")
 machine.run()
 '''
@@ -27,15 +28,15 @@ machine.run()
 '''
 #Binary to Decimal Convertion - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/binary_to_decimal_conversion.in")
-machine.insert_tape("1001")
+machine.load_program("../tests/binary_to_decimal_conversion.in")
+machine.insert_tape("10")
 machine.run()
 '''
 
 '''
 #Turing sequence machine - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/turings_sequence_machine.in")
+machine.load_program("../tests/turings_sequence_machine.in")
 machine.insert_tape("")
 machine.run()
 '''
@@ -43,7 +44,7 @@ machine.run()
 '''
 #Parentheses checker - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/parentheses_checker.in")
+machine.load_program("../tests/parentheses_checker.in")
 machine.insert_tape("12(2+(3^(4-1)))")
 machine.run()
 '''
@@ -51,7 +52,7 @@ machine.run()
 '''
 #Reverse Polish Boolean - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/reverse_polish_boolean.in")
+machine.load_program("../tests/reverse_polish_boolean.in")
 machine.insert_tape("10~1&|0|")
 machine.run()
 '''
@@ -59,7 +60,7 @@ machine.run()
 '''
 #Primality Test - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/primality_test.in")
+machine.load_program("../tests/primality_test.in")
 machine.insert_tape("101")
 machine.run()
 '''
@@ -67,7 +68,7 @@ machine.run()
 '''
 #4-State busy beaver  - ??
 machine = Machine()
-machine.upload_states_by_archive("../tests/4_state_busy_beaver.in")
+machine.load_program("../tests/4_state_busy_beaver.in")
 machine.insert_tape("")
 machine.run()
 '''
@@ -75,7 +76,7 @@ machine.run()
 '''
 #Universal Turing Machine - OK
 machine = Machine()
-machine.upload_states_by_archive("../tests/universal_turing_machine.in")
+machine.load_program("../tests/universal_turing_machine.in")
 machine.insert_tape("[ L+,0R.,1R.!1L+,1L+,0L.:,0L.,1L.:]1011")
 machine.run()
 '''
